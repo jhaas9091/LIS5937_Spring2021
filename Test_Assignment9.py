162bda4363a2d7af6470dc7128473c704afcf902
jhaas9091 <78804749+jhaas9091@users.noreply.github.com>
2021-03-10 13:58:06 -0500

    Add files via upload

---
+++ Test_Assignment9.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_equal_sets(self):
+        x = ['Thirteen', 13, 'Purple', 'tulip']
+        y = ['Thirteen', 13, 'Purple', 'pansy']
+        self.assertListEqual (x, y, msg = "Lists Are Not Equal")
+
+    def test_almost_equal(self):
+        x = 0.092
+        y = 0.09
+        self.assertAlmostEqual(x, y, msg = "Decimals Not Equal")
+
+    def test_boolean(self):
+        x = True
+        y = True
+        self.assertEqual(x, y, msg = "X and Y are Not Equal")
+
+if __name__ == '__main__':
+    unittest.main()
