550ae9ee9dbed837abaf42dc8878a01630bbb730
jhaas9091 <78804749+jhaas9091@users.noreply.github.com>
2021-03-18 16:20:01 -0400

    Update Assignment10_Matplotlib.py

--- Assignment10_Matplotlib.py
+++ Assignment10_Matplotlib.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv (
-    r"C:\Users\jbae1\OneDrive\UCF MLIS\LIS5937 - Python for Data Sci Prof\Week 10\Assign10\LifeExpectancy_Edited2.csv")
+    r"LifeExpectancy_Edited2.csv")
 
 df_x = pd.DataFrame(data, columns=['Year'])
 df_y = pd.DataFrame(data, columns=['Life Expectancy'])
