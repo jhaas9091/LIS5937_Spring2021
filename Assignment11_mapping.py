6d170e6c70cd483b991b4f54e206b3136c016641
jhaas9091 <78804749+jhaas9091@users.noreply.github.com>
2021-03-26 11:42:10 -0400

    Update Assignment11_mapping.py

--- Assignment11_mapping.py
+++ Assignment11_mapping.py
@@ -1,7 +1,14 @@
-# COVID Headers
+"""Module 10 Assignment
+Julie Haas
+LIS 5937
+Spring 2021"""
+
+# Covid Headers
 COUNTY = 3
 COUNTYNAME = 4
 PUIsTotal = 13
+
+# Age Headers
 Age_0_9 = 14
 Age_10_19 = 15
 Age_20_29 = 16
@@ -12,17 +19,3 @@ Age_60_69 = 20
 Age_70_79 = 21
 Age_80plus = 22
 Age_Unkn = 23
-
-'''# Product fields
-PRODUCT_ID = 3
-PRODUCT_PARENT = 4
-PRODUCT_TITLE = 5
-PRODUCT_CATEGORY = 6
-
-# Review fields
-REVIEW_ID = 2
-REVIEW_CUSTOMER = 1
-REVIEW_STARS = 7
-REVIEW_HEADLINE = 12
-REVIEW_BODY = 13
-REVIEW_DATE = 14'''
