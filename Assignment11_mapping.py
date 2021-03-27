9ab271005856777dfd4ffdf43c934d9d751fb511
jhaas9091 <78804749+jhaas9091@users.noreply.github.com>
2021-03-27 01:02:00 -0400

    Update Assignment11_mapping.py

--- Assignment11_mapping.py
+++ Assignment11_mapping.py
@@ -4,18 +4,18 @@ LIS 5937
 Spring 2021"""
 
 # Covid Headers
-COUNTY = 3
-COUNTYNAME = 4
-PUIsTotal = 13
+COUNTY = 0
+COUNTYNAME = 1
+PUIsTotal = 11
 
 # Age Headers
-Age_0_9 = 14
-Age_10_19 = 15
-Age_20_29 = 16
-Age_30_39 = 17
-Age_40_49 = 18
-Age_50_59 = 19
-Age_60_69 = 20
-Age_70_79 = 21
-Age_80plus = 22
-Age_Unkn = 23
+Age_0_9 = 12
+Age_10_19 = 13
+Age_20_29 = 14
+Age_30_39 = 15
+Age_40_49 = 16
+Age_50_59 = 17
+Age_60_69 = 18
+Age_70_79 = 19
+Age_80plus = 20
+Age_Unkn = 21
